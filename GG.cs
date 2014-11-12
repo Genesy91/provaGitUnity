5d706e547d0f4d943da013694102135800b701bb
Davide <damacchi91@gmail.com>
2014-11-12 18:08:53 +0100

    first
    first

---
+++ GG.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GG : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("GG wp");
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
